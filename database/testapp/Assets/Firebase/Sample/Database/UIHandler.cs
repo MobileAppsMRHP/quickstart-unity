cfbc315ec30eee0d2f11714ad2dca05366e88dea
D211\budak7273 <budak7273@students.d211.org>
2019-02-06 11:01:26 -0600

    Get example working

--- database/testapp/Assets/Firebase/Sample/Database/UIHandler.cs
+++ database/testapp/Assets/Firebase/Sample/Database/UIHandler.cs
@@ -65,7 +65,7 @@ namespace Firebase.Sample.Database {
       FirebaseApp app = FirebaseApp.DefaultInstance;
       // NOTE: You'll need to replace this url with your Firebase App's database
       // path in order for the database connection to work correctly in editor.
-      app.SetEditorDatabaseUrl("https://unity-test-app-fc4db.firebaseio.com/");
+      app.SetEditorDatabaseUrl("https://mobileappsmrhp-spacecolony.firebaseio.com/");
       if (app.Options.DatabaseUrl != null)
         app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
       StartListener();
